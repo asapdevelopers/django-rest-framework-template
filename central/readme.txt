5615f5cd7f9dd656b8c4593f438e67bba37546ac
Fabricio Duarte <fabricioaduarte@gmail.com>
2017-03-24 14:33:30 -0300

    Muevo los archivos al root (elimino la carpeta central)

--- central/readme.txt
+++
@@ -1,5 +0,0 @@
-- Delete db.sqlite3
-- Modify settings.py to change various settings, private keys, access keys, and project/app names. Make sure to find and replace all names
-- Run manage.py migrate
-- Run manage.py createsuperuser
-
--- central/urls.py
+++ central/urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.views.i18n import javascript_catalog
 from django.http import HttpResponse
-from django.conf import settings
-from clients import urls as clients_urls
+
 from administration import urls as administration_urls
+from clients import urls as clients_urls
 
 urlpatterns = []
 
